8537c54a0bba6e9b7524aee903b8923b2e979a6f
Won-dy <junhfireace@gmail.com>
2022-04-24 16:18:26 +0900

    Spring Boot 프로젝트 생성

---
+++ src/test/java/ramyeon/everyday/EverydayApplicationTests.java
@@ -0,0 +1,13 @@
+package ramyeon.everyday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverydayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
