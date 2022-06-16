11b1b2122eaddb9808126ad074a5d54811b89c6a
Won-dy <junhfireace@gmail.com>
2022-06-16 23:42:59 +0900

    Edit: CorsConfig 수정
    config.setAllowCredentials(true) 코드 삭제

--- src/main/java/ramyeon/everyday/config/CorsConfig.java
+++ src/main/java/ramyeon/everyday/config/CorsConfig.java
@@ -13,7 +13,6 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);  // 서버가 응답할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정
         config.addAllowedOrigin("*");  // 모든 ip에 응답을 허용
         config.addAllowedHeader("*");  // 모든 header에 응답을 허용
         config.addAllowedMethod("*");  // 모든 API method 요청을 허용
