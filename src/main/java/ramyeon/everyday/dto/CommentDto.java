36e5008c611f20215cc963ab06721d3842ed3e4a
Won-dy <junhfireace@gmail.com>
2022-07-12 22:35:03 +0900

    Edit: CommentDto에 작성자 아이디, 좋아요 수 추가

--- src/main/java/ramyeon/everyday/dto/CommentDto.java
+++ src/main/java/ramyeon/everyday/dto/CommentDto.java
@@ -7,11 +7,12 @@ import ramyeon.everyday.domain.Whether;
 import ramyeon.everyday.domain.comment.CommentType;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class CommentDto {
 
     /**
-     * 게시글 상세 조회의 댓글 DTO
+     * 댓글 조회 DTO
      */
     @Getter
     @Builder
@@ -19,7 +20,8 @@ public class CommentDto {
     public static class CommentResponseDto {
 
         private Long id;  // 댓글 ID
-        private String writer;  // 작성자
+        private String writer;  // 작성자(닉네임)
+        private String writerLoginId;  // 작성자 아이디
         private String contents;  // 내용
         private LocalDateTime registrationDate;  // 등록일시
         private CommentType commentType;  // 댓글 종류
@@ -27,7 +29,21 @@ public class CommentDto {
         private Whether isAnonymous;  // 익명 여부
 
         private Whether isLikeComment;  // 글을 조회한 회원의 댓글 좋아요 여부
+
+        private Long likeCount;  // 좋아요 수
     }
+
+    /**
+     * 댓글 조회 DTO
+     */
+    @Getter
+    @Builder
+    public static class CommentsDto {
+
+        private Integer commentCount;  // 댓글 수
+        private List<CommentResponseDto> comment;  // 댓글
+    }
+
 
     /**
      * 댓글 등록 DTO
